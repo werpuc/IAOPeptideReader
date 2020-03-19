b1fd1ff1fe431e063661365fa0e0d5abf8885fad
tmakowski <tymek.makowski@gmail.com>
2020-03-19 19:15:17 +0100

    Added TODOs for D3 plot development.

--- inst/shiny_app/www/plot.js
+++ inst/shiny_app/www/plot.js
@@ -1,1 +1,11 @@
-// TODO
+// TODO: create svg here:
+// svg = d3.select("#plot").append("svg")
+
+// TODO: create "g" for data objects:
+// svg.append("g").attr("id", "data_stuff")
+
+// TODO: add custom message handler for updating the data:
+// svg.selectAll("g#data_stuff > circle").data(data).join().[...]
+
+// TODO: add reactiveVal in R to trigger plot settings update on data filtering
+// (load plot_settings module in input_settings and pass the RV).
--- inst/shiny_app/www/plot_settings.js
+++ inst/shiny_app/www/plot_settings.js
@@ -1,3 +1,3 @@
 Shiny.addCustomMessageHandler("plot_title", function(plot_title) {
-    console.log(plot_title); // TODO: upate the plot's title.
+    console.log(plot_title); // TODO: upate the plot's title by using d3.select("#plot").[...].
 });
